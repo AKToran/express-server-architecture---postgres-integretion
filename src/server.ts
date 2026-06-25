63e25e158d4ce391dd13eacb4c3065e45bdbc1c4
AKToran <aktoran60@gmail.com>
2026-06-25 12:18:34 +0600

    postgres with neondb setup

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,40 @@
-import express, { type Application, type Request, type Response } from 'express';
-const app : Application = express()
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
+import { Pool } from "pg";
+
+const app: Application = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req : Request, res : Response) => {
+const pool = new Pool({
+  connectionString : ""
+})
+
+app.get("/", (req: Request, res: Response) => {
   // res.send('Hello World!');
   res.status(200).json({
-    "message" : "Express server.",
-    "author" : "Abid"
-  })
-})
+    message: "Express server.",
+    author: "Abid",
+  });
+});
 
-app.post('/', async(req: Request, res: Response) =>{
-  console.log(req.body);
-})
+app.post("/", async (req: Request, res: Response) => {
+  const { name, email, password } = req.body;
+  res.status(201).json({
+    message: "Created",
+    data: {
+      name,
+      email,
+    },
+  });
+});
 
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
-})
+  console.log(`App listening on port ${port}`);
+});
