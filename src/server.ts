8efed57b1d14b8d9e3193024942bc8bb1656d910
AKToran <aktoran60@gmail.com>
2026-06-26 04:08:19 +0600

    delete api to delete a user

--- src/server.ts
+++ src/server.ts
@@ -171,6 +171,40 @@ app.put("/api/users/:id", async (req: Request, res: Response) => {
   }
 });
 
+app.delete("/api/users/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    const result = await pool.query(
+      `
+      DELETE FROM users WHERE id=$1
+      `,
+      [id],
+    );
+
+    if (result.rowCount === 0) {
+      res.status(404).json({
+        success: false,
+        message: "User not found.",
+        data: null,
+      });
+      return;
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "User deleted successfully.",
+      data: {},
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      error: error,
+    });
+  }
+});
+
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
